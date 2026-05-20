0692f7892093e771e36fc62c7c37366b97cf86e0
Rupender Singh <rupender.singh@gmail.com>
2026-05-20 14:51:31 +0530

    operators and loops

---
+++ checks/student_grades.js
@@ -0,0 +1,17 @@
+let marks = 70
+console.log(marks);
+
+if (marks >= 90) {
+    console.log("Grade = A")
+}
+else if (marks >= 80 && marks <= 89) {
+    console.log("Grade = B")
+}
+else if (marks >= 70 && marks <= 79) {
+    console.log("Grade = C")
+}
+else if (marks >= 60 && marks <= 69) {
+    console.log("Grade = D")
+}
+else
+    console.log("Fail")
---
+++ checks/test.js
@@ -0,0 +1,5 @@
+greet();
+function greet() { console.log("Hi"); }
+
+//console.log(a)
+const a = 10;
